8f48d24e773a1b70dc8f7d19d143c23b8a4916b9
Daniel Saidi <daniel.saidi@gmail.com>
2017-08-20 23:33:15 +0200

    Add CocoaPods and abstract domain model

---
+++ DemoApplication/DemoApplication/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DemoApplication
+//
+//  Created by Saidi Daniel (BookBeat) on 2017-08-20.
+//  Copyright © 2017 Daniel Saidi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ DemoApplication/DemoApplication/Domain/Services/AuthService.swift
@@ -0,0 +1,17 @@
+//
+//  AuthService.swift
+//  DemoApplication
+//
+//  Created by Saidi Daniel (BookBeat) on 2017-08-20.
+//  Copyright © 2017 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+typealias AuthResult = (_ authToken: String?, _ error: Error?) -> ()
+
+
+protocol AuthService: class {
+    
+    func authorizeApplication(_ completion: @escaping AuthResult)
+}
